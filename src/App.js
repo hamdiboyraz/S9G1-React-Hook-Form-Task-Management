2aea2dca53c0738128df2fb1c6ecd866e6d2515a
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-03-23 15:19:29 +0300

    Add notification with toastify and handleComplete function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import "./app.css";
 import Task from "./Task";
 import TaskForm from "./TaskForm";
@@ -11,17 +12,60 @@ function App() {
   const [team, setTeam] = useState(initialTeam);
 
   function handleTaskSubmit(yeniTask) {
-    console.log(yeniTask);
-
     setTasks([yeniTask, ...tasks]);
+    toast(`${yeniTask.title} Yapılacaklara eklendi`);
   }
 
   function handlePeopleSubmit(yeniKisi) {
     setTeam([...team, yeniKisi]);
+    toast(`${yeniKisi} takıma eklendi`);
   }
 
   function handleComplete(id) {
-    console.log("tamamlama fonksiyonunu buraya yazın");
+    // OPT 1
+    // const newTasks = tasks.map((el) => {
+    //   return el.id === id ? { ...el, status: "yapıldı" } : el;
+    // });
+    // setTasks(newTasks);
+    // toast(`Görev tamamlandı`);
+    // OPT 2 : tek satırda yazıldı, return yok
+    // const newTasks = tasks.map((el) =>
+    //   el.id === id ? { ...el, status: "yapıldı" } : el
+    // );
+    // setTasks(newTasks);
+    // toast(`Görev tamamlandı`);
+    // OPT 3 : Direkt içinde güncellendi
+    // setTasks(
+    //   tasks.map((task) =>
+    //     task.id === id ? { ...task, status: "yapıldı" } : task
+    //   )
+    // );
+    // OPT 4 : Direkt içinde güncellendi, callback func yazıldı.
+    // setTasks((prevTasks) =>
+    //   prevTasks.map((task) =>
+    //     task.id === id ? { ...task, status: "yapıldı" } : task
+    //   )
+    // );
+    // OPT 5: Ayrı bir array oluşturuldu, ek olarak görev title yakalandı
+    // const newTasks = [...tasks];
+    // const completedTask = newTasks.find((el) => el.id === id);
+    // if (completedTask) {
+    //   completedTask.status = "yapıldı";
+    // }
+    // setTasks(newTasks);
+    // toast(`${completedTask.title} tamamlandı`);
+
+    // OPT 6: Toastlı hali
+    setTasks((prevTasks) =>
+      prevTasks.map((task) => {
+        if (task.id === id) {
+          toast(`${task.title} tamamlandı`);
+          return { ...task, status: "yapıldı" };
+        } else {
+          return task;
+        }
+      })
+    );
   }
 
   return (
@@ -60,6 +104,18 @@ function App() {
           </div>
         </div>
       </div>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="light"
+      />
     </div>
   );
 }
--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 import { useForm } from "react-hook-form";
 import { nanoid } from "nanoid";
 
